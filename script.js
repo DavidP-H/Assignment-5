3f82e9a68e508cded16662893a7f8d20b996592d
DavidHart05 <>
2021-05-06 13:08:05 +0000

    space

--- script.js
+++ script.js
@@ -5,35 +5,35 @@ document.getElementById('button').addEventListener('click', doArea)
 document.getElementById('button').addEventListener('click', doCircumference)
 
 function doCircumference () {
-// gets the input value
+  // gets the input value
   circumference = document.getElementById('input').value
 
-// converts it to a floating point number
+  // converts it to a floating point number
   circumference = parseFloat(circumference)
 
-// multiply by pi
+  // multiply by pi
   circumference = Math.PI * circumference
 
-// write to HTML
+  // write to HTML
   document.getElementById('circans').innerHTML = circumference
 }
 
 function doArea () {
-// gets the input value
+  // gets the input value
   area = document.getElementById('input').value
 
-// converts it to a floating point number
+  // converts it to a floating point number
   area = parseFloat(area)
 
-// divide by 2 for radius
+  // divide by 2 for radius
   area = area / 2
 
-// square it
+  // square it
   area = area * area
 
-// pi times r squared
+  // pi times r squared
   area = Math.PI * area
 
-// write to HTML
+  // write to HTML
   document.getElementById('areaans').innerHTML = area
 }
